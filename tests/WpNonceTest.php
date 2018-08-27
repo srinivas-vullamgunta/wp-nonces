79652fcf3a4570a96a9defeb4716e953f7a74b67
Srinivas <srinivas.vullamgunta@gmail.com>
2018-08-27 20:59:02 +0530

    comments to test functions

--- tests/WpNonceTest.php
+++ tests/WpNonceTest.php
@@ -15,12 +15,20 @@ class WpNonceTest extends \PHPUnit_Framework_TestCase
 	 */
 	private static $nonce;
 
+	/**
+	 * Test if action is not set.
+	 * 
+	 */
 	public function testSetAction() {
 		$expected = -1;
 		self::$nonce = new WpNonce();
 		$this->assertSame($expected, self::$nonce->setAction());
 	}
 
+	/**
+	 * Test verify nonce flag.
+	 * 
+	 */
 	public function testWpVerifyNonce() {  
 		self::$nonce = new WpNonce();
 		$nonceVerifyVal = '';
